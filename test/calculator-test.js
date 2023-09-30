58d3b79ad114b5b5f8c457a544d6623fd148621e
DESKTOP-28P4RBF\LENOVO <david_soto123@hotmail.com>
2023-09-30 01:04:55 -0400

    prueba unitaria

--- test/calculator-test.js
+++ test/calculator-test.js
@@ -6,8 +6,8 @@ var calculator = require('../calculator');
 describe('pruebas unitarias: ', function(){
     describe('verificando funcion addTest',function(){
             it('verificar valor retornado: assert.equal(val, val): ', function(){
-                resultl = calculator.addTest(1,1);
-                assert.equal(resultl, 2);
+                resutl = calculator.addTest(1,1);
+                assert.equal(resutl, 2);
 
 
             });
